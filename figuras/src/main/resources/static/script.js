3fd6749ba0c48510f21c79c5df176defe33b6dab
Lucas Piris <lucasgpiris@hotmail.com>
2025-06-16 18:28:01 -0300

    4.2 commit

--- figuras/src/main/resources/static/script.js
+++ figuras/src/main/resources/static/script.js
@@ -76,19 +76,3 @@ document.getElementById('circuloForm').addEventListener('submit', (e) => {
             document.getElementById('circulo-resultados').innerHTML = html;
         })
 })
-// Área, perímetro e hipotenusa
-// document.getElementById('areaForm').addEventListener('submit', function(e) {
-//     e.preventDefault();
-//     const base = document.getElementById('base').value;
-//     const altura = document.getElementById('altura').value;
-//     const radio = document.getElementById('radio').value;
-//     let url = `/area?base=${encodeURIComponent(base)}&altura=${encodeURIComponent(altura)}`;
-//     if (radio) {
-//         url += `&radio=${encodeURIComponent(radio)}`;
-//     }
-//     fetch(url)
-//         .then(res => res.text())
-//         .then(html => {
-//             document.getElementById('areaResultado').innerHTML = html;
-//         });
-// });
